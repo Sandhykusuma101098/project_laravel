5a1a791f84372f9f946f90093f5460cca778bd8f
Roy <mangappu.pardamean.simanjuntak19@mhs.ubharajaya.ac.id>
2022-04-30 18:21:38 +0700

    penambahan web route

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::prefix('admin')->group(function () {
 
     Route::get('/jsIntroduction', 'JsintroController@index')->name('jsintro');
     Route::get('/jsIntroduction_next', 'JsintroController@index_next')->name('jsintro_next');
+
+    Route::get('/jsSandhy', 'SandhyController@index')->name('jssandhy');
+    Route::get('/jsSandhy_next', 'SandhyController@index_next')->name('jssandhy_next');
   
 
 });
